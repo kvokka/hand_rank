d445f27ce68c7e42ac48058bc6e774b4a370ae70
kvokka <kvokka@yahoo.com>
2019-08-01 08:41:39 +0100

    Add rake data post auto-copy feature

--- ext/hand_rank/extconf.rb
+++ ext/hand_rank/extconf.rb
@@ -1,3 +1,19 @@
 require "mkmf"
 $CFLAGS+=" -O3"
 create_makefile "hand_rank/hand_rank"
+
+require 'pathname'
+
+data_file = Pathname.new(File.expand_path('..', __dir__)).join "../lib/hand_rank/ranks.data"
+
+begin
+  %x[gunzip -f #{data_file}.gz] unless File.exists?(data_file)
+rescue => e
+  puts <<-EOF
+  Something go wrong with copying ranks.data file from
+  #{data_file}
+  to gem folder
+
+  Please install it manually in path/to/hand_rank/lib/hand_hank
+  EOF
+end
